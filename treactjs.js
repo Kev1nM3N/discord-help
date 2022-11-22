45f50b1fd586c8c0b617a411e5ceccd153e43eee
Kevin Mendez <mediaman502@aol.com>
2022-11-22 15:34:16 -0500

    after tutor with zen, officially done!

--- treactjs.js
+++ treactjs.js
@@ -1,7 +1,11 @@
 function openMenu() {
-    document.body.classList += " menu--open"
-}
-
-function closeMenu() {
-    document.body.classList.remove('menu--open')
-}
+    // keep the 'open' classes, remove the 'closed' classes
+    document.body.classList.add("menu--open");
+    document.body.classList.remove("menu--close");
+  }
+  
+  function closeMenu() {
+    // keep the 'closed' classes, remove the 'open' classes
+    document.body.classList.remove("menu--open");
+    document.body.classList.add("menu--close");
+  }
